b6a5b238548572424d20c64f4bc99e071ad8df61
Jorge Calatayud Maeso <jorge.cmaeso@alumnos.upm.es>
2017-12-27 21:12:22 +0100

    Added GPIO tuts

---
+++ TestBtnInt/Debug/Src/system_stm32f0xx.d
@@ -0,0 +1,73 @@
+Src/system_stm32f0xx.o: ../Src/system_stm32f0xx.c \
+ C:/Users/jcala/Desktop/TestBtnInt/Drivers/CMSIS/Device/ST/STM32F0xx/Include/stm32f0xx.h \
+ C:/Users/jcala/Desktop/TestBtnInt/Drivers/CMSIS/Device/ST/STM32F0xx/Include/stm32f051x8.h \
+ C:/Users/jcala/Desktop/TestBtnInt/Drivers/CMSIS/Include/core_cm0.h \
+ C:/Users/jcala/Desktop/TestBtnInt/Drivers/CMSIS/Include/core_cmInstr.h \
+ C:/Users/jcala/Desktop/TestBtnInt/Drivers/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/jcala/Desktop/TestBtnInt/Drivers/CMSIS/Include/core_cmFunc.h \
+ C:/Users/jcala/Desktop/TestBtnInt/Drivers/CMSIS/Device/ST/STM32F0xx/Include/system_stm32f0xx.h \
+ C:/Users/jcala/Desktop/TestBtnInt/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal.h \
+ C:/Users/jcala/Desktop/TestBtnInt/Inc/stm32f0xx_hal_conf.h \
+ C:/Users/jcala/Desktop/TestBtnInt/Inc/main.h \
+ C:/Users/jcala/Desktop/TestBtnInt/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_rcc.h \
+ C:/Users/jcala/Desktop/TestBtnInt/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_def.h \
+ C:/Users/jcala/Desktop/TestBtnInt/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_rcc_ex.h \
+ C:/Users/jcala/Desktop/TestBtnInt/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_gpio.h \
+ C:/Users/jcala/Desktop/TestBtnInt/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_gpio_ex.h \
+ C:/Users/jcala/Desktop/TestBtnInt/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_dma.h \
+ C:/Users/jcala/Desktop/TestBtnInt/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_dma_ex.h \
+ C:/Users/jcala/Desktop/TestBtnInt/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_cortex.h \
+ C:/Users/jcala/Desktop/TestBtnInt/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_flash.h \
+ C:/Users/jcala/Desktop/TestBtnInt/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_flash_ex.h \
+ C:/Users/jcala/Desktop/TestBtnInt/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_i2c.h \
+ C:/Users/jcala/Desktop/TestBtnInt/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_i2c_ex.h \
+ C:/Users/jcala/Desktop/TestBtnInt/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_pwr.h \
+ C:/Users/jcala/Desktop/TestBtnInt/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_pwr_ex.h
+
+C:/Users/jcala/Desktop/TestBtnInt/Drivers/CMSIS/Device/ST/STM32F0xx/Include/stm32f0xx.h:
+
+C:/Users/jcala/Desktop/TestBtnInt/Drivers/CMSIS/Device/ST/STM32F0xx/Include/stm32f051x8.h:
+
+C:/Users/jcala/Desktop/TestBtnInt/Drivers/CMSIS/Include/core_cm0.h:
+
+C:/Users/jcala/Desktop/TestBtnInt/Drivers/CMSIS/Include/core_cmInstr.h:
+
+C:/Users/jcala/Desktop/TestBtnInt/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+C:/Users/jcala/Desktop/TestBtnInt/Drivers/CMSIS/Include/core_cmFunc.h:
+
+C:/Users/jcala/Desktop/TestBtnInt/Drivers/CMSIS/Device/ST/STM32F0xx/Include/system_stm32f0xx.h:
+
+C:/Users/jcala/Desktop/TestBtnInt/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal.h:
+
+C:/Users/jcala/Desktop/TestBtnInt/Inc/stm32f0xx_hal_conf.h:
+
+C:/Users/jcala/Desktop/TestBtnInt/Inc/main.h:
+
+C:/Users/jcala/Desktop/TestBtnInt/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_rcc.h:
+
+C:/Users/jcala/Desktop/TestBtnInt/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_def.h:
+
+C:/Users/jcala/Desktop/TestBtnInt/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_rcc_ex.h:
+
+C:/Users/jcala/Desktop/TestBtnInt/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_gpio.h:
+
+C:/Users/jcala/Desktop/TestBtnInt/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_gpio_ex.h:
+
+C:/Users/jcala/Desktop/TestBtnInt/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_dma.h:
+
+C:/Users/jcala/Desktop/TestBtnInt/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_dma_ex.h:
+
+C:/Users/jcala/Desktop/TestBtnInt/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_cortex.h:
+
+C:/Users/jcala/Desktop/TestBtnInt/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_flash.h:
+
+C:/Users/jcala/Desktop/TestBtnInt/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_flash_ex.h:
+
+C:/Users/jcala/Desktop/TestBtnInt/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_i2c.h:
+
+C:/Users/jcala/Desktop/TestBtnInt/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_i2c_ex.h:
+
+C:/Users/jcala/Desktop/TestBtnInt/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_pwr.h:
+
+C:/Users/jcala/Desktop/TestBtnInt/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_pwr_ex.h:
---
+++ TestMultipleInts/Debug/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal.d
@@ -0,0 +1,74 @@
+Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal.o: \
+ ../Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal.c \
+ C:/Users/jcala/Desktop/TestMultipleInts/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal.h \
+ C:/Users/jcala/Desktop/TestMultipleInts/Inc/stm32f0xx_hal_conf.h \
+ C:/Users/jcala/Desktop/TestMultipleInts/Inc/main.h \
+ C:/Users/jcala/Desktop/TestMultipleInts/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_rcc.h \
+ C:/Users/jcala/Desktop/TestMultipleInts/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_def.h \
+ C:/Users/jcala/Desktop/TestMultipleInts/Drivers/CMSIS/Device/ST/STM32F0xx/Include/stm32f0xx.h \
+ C:/Users/jcala/Desktop/TestMultipleInts/Drivers/CMSIS/Device/ST/STM32F0xx/Include/stm32f051x8.h \
+ C:/Users/jcala/Desktop/TestMultipleInts/Drivers/CMSIS/Include/core_cm0.h \
+ C:/Users/jcala/Desktop/TestMultipleInts/Drivers/CMSIS/Include/core_cmInstr.h \
+ C:/Users/jcala/Desktop/TestMultipleInts/Drivers/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/jcala/Desktop/TestMultipleInts/Drivers/CMSIS/Include/core_cmFunc.h \
+ C:/Users/jcala/Desktop/TestMultipleInts/Drivers/CMSIS/Device/ST/STM32F0xx/Include/system_stm32f0xx.h \
+ C:/Users/jcala/Desktop/TestMultipleInts/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_rcc_ex.h \
+ C:/Users/jcala/Desktop/TestMultipleInts/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_gpio.h \
+ C:/Users/jcala/Desktop/TestMultipleInts/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_gpio_ex.h \
+ C:/Users/jcala/Desktop/TestMultipleInts/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_dma.h \
+ C:/Users/jcala/Desktop/TestMultipleInts/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_dma_ex.h \
+ C:/Users/jcala/Desktop/TestMultipleInts/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_cortex.h \
+ C:/Users/jcala/Desktop/TestMultipleInts/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_flash.h \
+ C:/Users/jcala/Desktop/TestMultipleInts/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_flash_ex.h \
+ C:/Users/jcala/Desktop/TestMultipleInts/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_i2c.h \
+ C:/Users/jcala/Desktop/TestMultipleInts/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_i2c_ex.h \
+ C:/Users/jcala/Desktop/TestMultipleInts/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_pwr.h \
+ C:/Users/jcala/Desktop/TestMultipleInts/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_pwr_ex.h
+
+C:/Users/jcala/Desktop/TestMultipleInts/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal.h:
+
+C:/Users/jcala/Desktop/TestMultipleInts/Inc/stm32f0xx_hal_conf.h:
+
+C:/Users/jcala/Desktop/TestMultipleInts/Inc/main.h:
+
+C:/Users/jcala/Desktop/TestMultipleInts/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_rcc.h:
+
+C:/Users/jcala/Desktop/TestMultipleInts/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_def.h:
+
+C:/Users/jcala/Desktop/TestMultipleInts/Drivers/CMSIS/Device/ST/STM32F0xx/Include/stm32f0xx.h:
+
+C:/Users/jcala/Desktop/TestMultipleInts/Drivers/CMSIS/Device/ST/STM32F0xx/Include/stm32f051x8.h:
+
+C:/Users/jcala/Desktop/TestMultipleInts/Drivers/CMSIS/Include/core_cm0.h:
+
+C:/Users/jcala/Desktop/TestMultipleInts/Drivers/CMSIS/Include/core_cmInstr.h:
+
+C:/Users/jcala/Desktop/TestMultipleInts/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+C:/Users/jcala/Desktop/TestMultipleInts/Drivers/CMSIS/Include/core_cmFunc.h:
+
+C:/Users/jcala/Desktop/TestMultipleInts/Drivers/CMSIS/Device/ST/STM32F0xx/Include/system_stm32f0xx.h:
+
+C:/Users/jcala/Desktop/TestMultipleInts/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_rcc_ex.h:
+
+C:/Users/jcala/Desktop/TestMultipleInts/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_gpio.h:
+
+C:/Users/jcala/Desktop/TestMultipleInts/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_gpio_ex.h:
+
+C:/Users/jcala/Desktop/TestMultipleInts/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_dma.h:
+
+C:/Users/jcala/Desktop/TestMultipleInts/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_dma_ex.h:
+
+C:/Users/jcala/Desktop/TestMultipleInts/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_cortex.h:
+
+C:/Users/jcala/Desktop/TestMultipleInts/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_flash.h:
+
+C:/Users/jcala/Desktop/TestMultipleInts/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_flash_ex.h:
+
+C:/Users/jcala/Desktop/TestMultipleInts/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_i2c.h:
+
+C:/Users/jcala/Desktop/TestMultipleInts/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_i2c_ex.h:
+
+C:/Users/jcala/Desktop/TestMultipleInts/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_pwr.h:
+
+C:/Users/jcala/Desktop/TestMultipleInts/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_pwr_ex.h:
